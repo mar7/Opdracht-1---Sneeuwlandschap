2f3f3681e56dc8672dadeb6212c79674c42f5e80
mar7 <marijn0@hotmail.com>
2012-04-13 09:43:59 +0200

    sneeuwvlok klasse

---
+++ SneeuwVlok.cpp
@@ -0,0 +1,36 @@
+/*
+ * SneeuwVlok.cpp
+ *
+ *  Created on: 13 apr. 2012
+ *      Author: Marijn
+ */
+
+#include "SneeuwVlok.hpp"
+#include <ma.h>
+
+SneeuwVlok::SneeuwVlok( int positionX, int positionY )
+{
+
+}
+
+SneeuwVlok::~SneeuwVlok()
+{
+	// destructor
+}
+
+void SneeuwVlok::draw()	//teken functie die een sneeuwvlok tekent
+{
+
+}
+
+void SneeuwVlok::fall( int groundLevel ) //functie die de y positie verandert
+{
+
+}
+
+bool SneeuwVlok::isDead()
+{
+
+}
+
+
